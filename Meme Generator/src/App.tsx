b0c75ed58f60ce99a8b905dfd35653a15c3564b9
Fatih <windahbatukdarah@gmail.com>
2026-06-10 13:18:24 +0700

    Delete Meme Generator directory

--- Meme Generator/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import MemeGenerator from "./components/MemeGenerator";
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 text-gray-900 font-sans selection:bg-purple-200 selection:text-purple-900">
-      <MemeGenerator />
-    </div>
-  );
-}
